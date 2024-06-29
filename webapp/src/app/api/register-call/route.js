f1ae43afa796293994e6ff66f160657890e253ee
life-efficient <harryaberg@gmail.com>
2024-06-29 17:25:33 +0700

    Implements retell sdk implementation from docs for server

--- webapp/src/app/api/register-call/route.js
+++ webapp/src/app/api/register-call/route.js
@@ -5,7 +5,5 @@ export async function POST(req, res) {
     console.log('calling register-call')
     console.log(req.method)
     const response = await registerCall(req.body);
-    failedtoregistercallonserver
-    console.log('response:', response)
     return NextResponse.json(response)
 }
--- webapp/src/utils/server/retell.js
+++ webapp/src/utils/server/retell.js
@@ -1,27 +1,39 @@
-// utils/server/retell.js
+import Retell from 'retell-sdk';
 
 export async function registerCall({ agent_id, audio_websocket_protocol, audio_encoding, sample_rate, end_call_after_silence_ms, from_number, to_number, direction, metadata, retell_llm_dynamic_variables }) {
 
     console.log('key:', process.env.RETELL_API_KEY)
-    const response = await fetch('https://api.retellai.com/register-call', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${process.env.RETELL_API_KEY}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            agent_id,
-            audio_websocket_protocol,
-            audio_encoding,
-            sample_rate,
-            end_call_after_silence_ms,
-            from_number,
-            to_number,
-            direction,
-            metadata,
-            retell_llm_dynamic_variables
-        })
+    const retell = new Retell({
+        apiKey: process.env.RETELL_API_KEY,
     });
+    const response = await retell.call.register({
+        agent_id: 'oBeDLoLOeuAbiuaMFXRtDOLriTJ5tSxD',
+        audio_encoding: 's16le',
+        audio_websocket_protocol: 'twilio',
+        sample_rate: 24000,
+    });
+
+    console.log(response.agent_id);
+    // const response = await fetch('https://api.retellai.com/register-call', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Authorization': `Bearer ${process.env.RETELL_API_KEY}`,
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         agent_id,
+    //         audio_websocket_protocol,
+    //         audio_encoding,
+    //         sample_rate,
+    //         end_call_after_silence_ms,
+    //         from_number,
+    //         to_number,
+    //         direction,
+    //         metadata,
+    //         retell_llm_dynamic_variables
+    //     })
+    // });
+    console.log('response:', response)
 
     return response
 }
