211ab6f115a72deeb78557904db9a37d86b63bf7
life-efficient <harryaberg@gmail.com>
2024-07-01 18:36:13 +0700

    Default agent

--- vercel-assistant-chat/agents/index.js
+++ vercel-assistant-chat/agents/index.js
@@ -1,10 +1,30 @@
 export default [
     {
+        agentId: 'csdcw90d9c9c9c9c9c9c9c9c9c9c9c9c9',
+        name: 'General Purpose Agent',
+        description: "A general purpose assistant to help you with a wide range of topics",
+        instructions: `
+            You are an agent designed to help you with a wide range of topics. Users can ask you questions about anything they like. 
+        `,
+        exampleMessages: [
+            {
+                heading: 'Help me to',
+                subheading: 'write a letter',
+                message: "Help me to write a letter"
+            },
+            {
+                heading: 'Help me to',
+                subheading: 'invite my friends to a party',
+                message: "Help me to invite my friends to a party"
+            },
+        ]
+    }
+    {
         agentId: 'a355652343eab572087abc77adfecb34',
         name: 'Executive Business Study Agent',
         description: "A friendly assistant to help you with your business studies",
         instructions: `
-            This agent is designed to help you with your business studies. You can ask it questions about business concepts, theories, and more. 
+            You are an agent designed to help you with your business studies. Users can ask you questions about business concepts, theories, and more. 
         `,
         exampleMessages: [
             {
