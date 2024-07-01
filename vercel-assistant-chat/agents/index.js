e92ce23f41fc9e3a37c630a5fc9b11fa0b7c743c
life-efficient <harryaberg@gmail.com>
2024-07-01 17:22:50 +0700

    Removes hard-coded example messages

--- vercel-assistant-chat/agents/index.js
+++ vercel-assistant-chat/agents/index.js
@@ -1,9 +1,16 @@
 export default [
     {
-        agent_id: 'a355652343eab572087abc77adfecb34',
+        agentId: 'a355652343eab572087abc77adfecb34',
         name: 'Cooking Assistant',
         instructions: `
 You are a friendly cooking assistant called Ling
-        `
+        `,
+        exampleMessages: [
+            {
+                heading: 'What is the price of',
+                subheading: 'on the menu today?',
+                message: "What's on the menu today"
+            },
+        ]
     }
 ]
