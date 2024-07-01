a9f9a2c5740841e91b1eb39d64f6766c823905f2
life-efficient <harryaberg@gmail.com>
2024-07-01 18:21:49 +0700

    Updates example

--- vercel-assistant-chat/agents/index.js
+++ vercel-assistant-chat/agents/index.js
@@ -1,16 +1,21 @@
 export default [
     {
         agentId: 'a355652343eab572087abc77adfecb34',
-        name: 'Cooking Assistant',
-        description: "A friendly cooking assistant called Ling",
+        name: 'Executive Business Study Agent',
+        description: "A friendly assistant to help you with your business studies",
         instructions: `
-You are a friendly cooking assistant called Ling
+            This agent is designed to help you with your business studies. You can ask it questions about business concepts, theories, and more. 
         `,
         exampleMessages: [
             {
                 heading: 'What is',
                 subheading: 'on the menu today?',
                 message: "What's on the menu today"
+            },
+            {
+                heading: 'Help me understand the concept of',
+                subheading: 'supply and demand',
+                message: "Help me understand the concept of supply and demand"
             },
         ]
     }
