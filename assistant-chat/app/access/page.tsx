f3a507a25ff9f4ee0257906166bbf6afcca2c164
life-efficient <harryaberg@gmail.com>
2024-07-06 01:10:41 +0700

    Removes unused import

--- assistant-chat/app/access/page.tsx
+++ assistant-chat/app/access/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useSearchParams, useRouter } from "next/navigation"
+import { useRouter } from "next/navigation"
 
 const StoreAccessToken = () => {
     // http://localhost:3000/access#state=state_parameter_passthrough_value&access_token=ya29.a0AXooCgtqWKVEVHo7xMTEA9GIjtzTSHZbZhtce9EPrSHHFu03Jsl3vc1BjrQFWPwkEDNmwmNqpbQW4XoyL76gZb5zNC5MvoROPw1arM0GORkyJe4G0g1L_1PEBpH9OUmUZu5W0L_TcobxmVc62_0oukwIUHGG2CJQ0pUaCgYKAW0SARMSFQHGX2MitwBTHSJtZDDCGN3mzup7rQ0170&token_type=Bearer&expires_in=3599&scope=https://www.googleapis.com/auth/spreadsheets%20https://www.googleapis.com/auth/calendar%20https://www.googleapis.com/auth/drive
