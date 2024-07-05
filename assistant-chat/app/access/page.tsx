60ddbcda2d7d69308ca3be7e2ac5a018ce5fc644
life-efficient <harryaberg@gmail.com>
2024-07-06 01:02:18 +0700

    Adds redirect and access token storage

--- assistant-chat/app/access/page.tsx
+++ assistant-chat/app/access/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+import { useSearchParams, useRouter } from "next/navigation"
 
 const StoreAccessToken = () => {
     // get url query params
+    const params = useSearchParams()
     // store access token in localstorage
+    localStorage.setItem('access_token', params.get('access_token'))
     // redirect back to correct agent and state
+    const router = useRouter()
+    router.push('/chat/calendar')
     return "hello world"
 }
 
