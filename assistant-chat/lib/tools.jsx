e148a2b47ce623b9b4ae940d59a49a57f12c0826
Owen Lee Scott <Fumwa23@outlook.com>
2024-07-08 01:04:16 +0800

    moved example code to legaceDemoTools

--- assistant-chat/lib/tools.jsx
+++ assistant-chat/lib/tools.jsx
@@ -452,63 +452,6 @@ async function makeToolApiRequest(accessToken, endpoint, payload = null, method 
     }
 }
 
-
-
-// IDEAL OUTPUT TOOL TEMPLATE for copilot TO COPY
-// {
-//         description: 'Example tool',
-//         parameters: z.object({
-//           example: z.string().describe('Example parameter')
-//         }),
-//         generate: async function* ({ example }) {
-//           yield (
-//             <BotCard>
-//               <p>Example tool</p>
-//             </BotCard>
-//           )
-
-//           await sleep(1000)
-
-//           const toolCallId = nanoid()
-
-//           aiState.done({
-//             ...aiState.get(),
-//             messages: [
-//               ...aiState.get().messages,
-//               {
-//                 id: nanoid(),
-//                 role: 'assistant',
-//                 content: [
-//                   {
-//                     type: 'tool-call',
-//                     toolName: 'exampleTool',
-//                     toolCallId,
-//                     args: { example }
-//                   }
-//                 ]
-//               },
-//               {
-//                 id: nanoid(),
-//                 role: 'tool',
-//                 content: [
-//                   {
-//                     type: 'tool-result',
-//                     toolName: 'exampleTool',
-//                     toolCallId,
-//                     result: example
-//                   }
-//                 ]
-//               }
-//             ]
-//           })
-
-//           return (
-//             <BotCard>
-//               <p>Example tool</p>
-//             </BotCard>
-//           )
-//         }
-
 export function ToolCallCompleteMessage({text}) {
     return (
         <div className="group relative flex items-start md:-ml-12">
